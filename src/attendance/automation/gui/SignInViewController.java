b22711353115f5e419f4c374dc2e1d2a7dc42896
Lumby98 <lumby89@gmail.com>
2020-02-12 16:30:34 +0100

    signin working kinda
    kan ikke åbne studentMainView, ser ud som det har noget at gøre med et billede som den ikke kan finde

--- src/attendance/automation/gui/SignInViewController.java
+++ src/attendance/automation/gui/SignInViewController.java
@@ -5,15 +5,21 @@
  */
 package attendance.automation.gui;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 
 /**
  *
@@ -30,19 +36,62 @@ public class SignInViewController implements Initializable
     @FXML
     private Label label;
     
+    private String userStudent;
+    private String passStudent;
+    private String userTeacher;
+    private String passTeacher;
+    
     @Override
     public void initialize(URL url, ResourceBundle rb)
     {
-        // TODO
+        userStudent = "Mads";
+        passStudent = "jensen";
+        
+        userTeacher = "Jeppe";
+        passTeacher = "baby";
     }    
 
     @FXML
-    private void handleSignIn(ActionEvent event)
+    private void handleSignIn(ActionEvent event) throws IOException
     {
-        String code = password.getText();
-        label.setText(code);
+      String user = username.getText();
+      String pass = password.getText();
+      
+      if(user.equals(userStudent) && pass.equals(passStudent))
+      {
+          //signIn student
+          FXMLLoader fxmlLoader = new FXMLLoader();
+         
+            Parent root = (Parent) fxmlLoader.load(getClass().getResource("StudentMainView.fxml").openStream());
+            StudentMainViewController cont = (StudentMainViewController) fxmlLoader.getController();
+            Stage stage = new Stage();
+            stage.setTitle("Attendance - Student");
+            stage.setScene(new Scene(root));
+            stage.show();
+          
+      }
+      else if(user.equals(userTeacher) && pass.equals(passTeacher))
+      {
+          //signIn teacher
+          FXMLLoader fxmlLoader = new FXMLLoader();
+          
+          Parent root = (Parent) fxmlLoader.load(getClass().getResource("TeacherMainView.fxml").openStream());
+          TeacherMainViewController cont = (TeacherMainViewController) fxmlLoader.getController();
+          Stage stage = new Stage();
+          stage.setTitle("Attendance - Teacher");
+          stage.setScene(new Scene(root));
+          stage.show();
+          
+      }
+      else
+      {
+          Alert alert = new Alert(Alert.AlertType.INFORMATION);
+                alert.setTitle("error");
+                alert.setHeaderText("oops, something went wrong");
+                alert.setContentText("Wrong username or password, please try again");
+                alert.showAndWait();
+      }
         
     }
-
     
 }
